fd72af8edc9307fb095b87aaff6175e618e89778
devifin <ba09d3@gmail.com>
2024-08-23 10:17:22 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'  // `router`のインポート
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)  // `router`をVueインスタンスに追加
+  .mount('#app')
